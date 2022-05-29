e4e402b86abfb4aa2571219f81327c58a84b8f9f
ben <ben.transinh@gmail.com>
2022-05-29 21:09:36 +0700

    install express

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    return res.send('Hello, world');
+})
+
+
+// 127.0.0.1
+//localhost
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
